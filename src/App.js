7b248c9d003e09fd1b132026bc7a1c69b82449b3
ANRoebuck <ANRoebuck@googlemail.com>
2019-07-25 12:33:16 +0100

    changed app function to class

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+
+class App extends Component {
+
+  render() {
+
+    return (
+      <div className="App">
       <header className="App-header">
         <p>
           test app for push notifications
         </p>
+        <button onClick={this.pingServer}>Ping server</button>
       </header>
     </div>
-  );
+    );
+
+  };
+
+  pingServer() {
+    console.log('pinging server...')
+  };
+
 }
+
+
+  
 
 export default App;
